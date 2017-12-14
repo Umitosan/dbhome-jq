fa853153f116af6160f6f137c116f9008c480b9a
Dominic Brown <yeahdom@gmail.com>
2017-12-13 23:14:52 -0800

    add scroll-to button tests

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 
+// custom jquery scroll function
+$.fn.scrollView = function() {
+  return this.each(function() {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+  });
+}
+
+
+
+
+
 $(document).ready(function() {
   console.log("<PAGE LODED>");
 
-  makeBoxes(8);
+  $('#p1').click(function() {
+    $('#panel-1').scrollView();
+  });
+  $('#p2').click(function() {
+    $('#panel-2').scrollView();
+  });
+  $('#p3').click(function() {
+    $('#panel-3').scrollView();
+  });
 
 })
